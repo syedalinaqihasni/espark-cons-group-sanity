ffd57822a570ac90affa501cfdb822a1bc07a886
Syed Ali Naqi Hasni <engr.syedalinaqihasni@gmail.com>
2026-03-06 09:49:29 +0500

    Add index.ts

---
+++ schemas/index.ts
@@ -0,0 +1,8 @@
+import blockContent from './blockContent'
+import blogCategory from './blogCategory'
+import blogTag from './blogTag'
+import blog from './blog'
+import job from './job'
+import seoContent from './seoContent'
+
+export const schemaTypes = [blockContent, blogCategory, blogTag, blog, job, seoContent ]
