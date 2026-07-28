716b61b8f57076f9467d92651cf5abe47f9717ca
Syed Ali Naqi Hasni <engr.syedalinaqihasni@gmail.com>
2026-07-29 03:47:07 +0500

    converted js to ts

--- sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '958pip7i',
-    dataset: 'production'
-  }
-})
--- schemas/index.js
+++
@@ -1,8 +0,0 @@
-import blockContent from './blockContent'
-import blogCategory from './blogCategory'
-import blogTag from './blogTag'
-import blog from './blog'
-import job from './job'
-import seoContent from './seoContent'
-
-export const schemaTypes = [blockContent, blogCategory, blogTag, blog, job, seoContent ]
